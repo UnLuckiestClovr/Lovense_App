6cfd67fd0481cb2a933bb669bd0652daed182676
UnLuckiestClovr <urluckyclovr@gmail.com>
2023-10-14 05:49:38 -0600

    10/14/2023 : 5:49 am ~ Tkinter Start

--- Python/API_Handling.py
+++ Python/API_Handling.py
@@ -18,8 +18,6 @@ def getToys():
 
         response.json()
         response.status_code
-
-        print("We Successfully Got to the End")
     except Exception as error:
         print(f"Error Found: {error}")
 
@@ -35,23 +33,162 @@ def getToyName():
         response.json()
         response.status_code
 
-        print("We Successfully Got to the End")
-
     except Exception as error:
         print(f"Error Found: {error}")
 
 
-def CommandCertainToy():
-    toyCommand = {
-        "command": "Function"
+# V V Command Setup V V
+"""
+
+    command: [required]
+        Type of request
+        Var = String
+
+    action: 
+        Control the function and strength of the toy
+        Var = string
+        Actions can be Vibrate, Rotate, Pump, Thrusting, Fingering, Suction or Stop. Use All to make all functions respond. Use Stop to stop the toy’s response.
+        Range:
+        Vibrate:0 ~ 20
+        Rotate: 0~20
+        Pump:0~3
+        Thrusting:0~20
+        Fingering:0~20
+        Suction:0~20
+        All:0~20
+
+    timeSec: [required]
+        Total running time
+        Var = double
+        0 = indefinite length
+        Otherwise, running time should be greater than 1.
+
+    loopRunningSec: [optional]
+        Running time
+        Var = double
+        Should be greater than 1
+
+    loopPauseSec: [optional]
+        Suspend Time
+        Var = double
+        Should be greater than 1
+
+    toy: [optional]
+        Toy ID
+        Var = string
+        If you do not include this, it will be applied to all toys
+
+    stopPrevious: [optional]
+        Stop all previous commands and execute current commands
+        Var = int
+        Default: 1, If set to 0 , it will not stop the previous command.
+
+    apiVer: [required]
+        The version of the request
+        Var = int
+        Always Uses 1
+"""
+
+#Examples
+"""
+    // Vibrate toy ff922f7fd345 at 16th strength, run 9 seconds then suspend 4 seconds. It will be looped. Total running time is 20 seconds.
+    {
+    "command": "Function",
+    "action": "Vibrate:16",
+    "timeSec": 20,
+    "loopRunningSec": 9,
+    "loopPauseSec": 4,
+    "toy": "ff922f7fd345",
+    "apiVer": 1
     }
 
-def CommandAllToys():
-    toysCommand = {
-        "command": "Funciton"
+    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+    // Vibrate 9 seconds at 2nd strength
+    // Rotate toys 9 seconds at 3rd strength
+    // Pump all toys 9 seconds at 4th strength
+    // For all toys, it will run 9 seconds then suspend 4 seconds. It will be looped. Total running time is 20 seconds.
+    {
+    "command": "Function",
+    "action": "Vibrate:2,Rotate:3,Pump:3",
+    "timeSec": 20,
+    "loopRunningSec": 9,
+    "loopPauseSec": 4,
+    "apiVer": 1
     }
+
+    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+    // Vibrate 9 seconds at 2nd strength
+    // The rest of the functions respond to 10th strength 9 seconds
+    {
+    "command": "Function",
+    "action": "Vibrate:2,All:10",
+    "timeSec": 20,
+    "loopRunningSec": 9,
+    "loopPauseSec": 4,
+    "apiVer": 1
+    }
+
+    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    
+    // Stop all toys
+    {
+    "command": "Function",
+    "action": "Stop",
+    "timeSec": 0,
+    "apiVer": 1
+    }
+"""
+
+def CommandToy_HUSH(funcSet):
+
+    toyCommand = {
+        "command": "Function",
+        "action": f"Vibrate:{str(funcSet['vibrateIntens'])}",
+        "timeSec": funcSet['timeSec'],
+        "loopRunningSec": funcSet['loopRunningSec'],
+        "loopPauseSec": funcSet['loopPauseSec'],
+        "apiVer": 1
+    }
+
+    response = requests.post(url, json=toyCommand)
+    print(response.json())
+
+    response.status_code
+    
+
+def CommandAllToys(funcSet):
+    toyCommand = {
+        "command": "Function",
+        "action": f"All:{str(funcSet['vibrateIntens'])}",
+        "timeSec": funcSet['timeSec'],
+        "loopRunningSec": funcSet['loopRunningSec'],
+        "loopPauseSec": funcSet['loopPauseSec'],
+        "apiVer": 1
+    }
+
+    response = requests.post(url, json=toyCommand)
+    print(response.json())
+
+    response.status_code
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
 
-getToys()
+def liveChanges(Strength: float):
+    toyCommand = {
+        "command": "Pattern",
+        "rule": "V:1;F:v,r,p,f,s,t;S:200#",
+        "strength": f"{Strength}",
+        "timeSec": 9,
+        "toy": "ff922f7fd345",
+        "apiVer": 2
+    }
+
+    response = requests.post(url, json=toyCommand)
+    print(response.json())
+
+    response.status_code
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
 
--- Python/Main_App.py
+++ Python/Main_App.py
@@ -1,2 +1,83 @@
 import os
 import API_Handling
+import tkinter as tk
+from tkinter import filedialog
+
+
+GlobalInfo = {
+    "userID": "",
+    "aviID": ""
+}
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
+"""
+    Tkinter Functions
+"""
+
+def browseFiles():
+    filename = filedialog.askopenfilename(initialdir = "/",
+                                          title = "Select a File",
+                                          filetypes = (("Text files",
+                                                        "*.txt*"),
+                                                       ("all files",
+                                                        "*.*")))
+      
+    # Change label contents
+    label_file_explorer.configure(text="File Opened: "+filename)
+
+def validateAviFile():
+    print("")
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
+"""
+    VRCHAT OSC SETUP by C.J.L.
+"""
+# aviFile = f"C:\Users\Momo\AppData\LocalLow\VRChat\VRChat\OSC\{GlobalInfo['userID']}\Avatars\{GlobalInfo['aviID']}.json"
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
+BahnBold = ("Bahnschrift Semibold", 22)
+BahnSemiBold = ("Bahnschrift SemiCondensed", 15)
+BahnSemiBoldCondensed = ("Bahnschrift SemiBold Condensed", 13)
+
+# Create the main application window
+root = tk.Tk()
+root.title("Euphoric_PyLove written by C.J.L.")
+root.config(bg='#2e2e2e')
+
+# Set the window size to 600x800
+root.geometry("600x800")
+
+# Create a label widget
+label = tk.Label(root, text="Hello, Welcome to Py-Love!", bg='#2e2e2e', fg="white", font=BahnBold)
+label.pack()  # Pack the label widget to fit the window
+
+labelRequest1 = tk.Label(root, font=BahnSemiBold, bg='#2e2e2e', fg="white", text="Please copy and select the path for your VRCHAT Avi,\nthen paste it in the below text box.\n\nPath Looks Similar to This:")
+labelRequest1.pack()
+
+labelRequest2 = tk.Label(root, font=BahnSemiBoldCondensed, bg='#2e2e2e', fg="white", text="C:\\Users\\<User>\\AppData\\LocalLow\\VRChat\\VRChat\\OSC\\<User_ID>\\Avatars\\<Avatar_ID>.json")
+labelRequest2.pack()
+
+labelRequest3 = tk.Label(root, font=BahnSemiBold, bg='#2e2e2e', fg="white", text="Your UserID Can be found in your URL at 'vrchat.com'.\nYour AvatarID can be found in the VRC Avatar Descriptor Component in Unity.")
+labelRequest3.pack()
+
+label_file_explorer = tk.Entry(root, 
+                            text = "File Explorer using Tkinter",
+                            width = 100,
+                            fg = "blue")
+label_file_explorer.pack()
+  
+      
+button_explore = tk.Button(root, 
+                        text = "Browse Files",
+                        command = browseFiles) 
+button_explore.pack()
+
+# Define a function to be called when the button is clicked
+def on_button_click():
+    label.config(text="Button Clicked!")
+
+# Start the main event loop
+root.mainloop()
