e46c3dabe2f5c330e078db79667a2cef30fcd08f
UnLuckiestClovr <urluckyclovr@gmail.com>
2023-10-14 22:37:41 -0600

    PyLove - 10/14/2023 ~ 10:38 pm

--- Python/PyLove/Py_Love_RR/RR_Surprise_PyLove.py
+++ Python/PyLove/Py_Love_RR/RR_Surprise_PyLove.py
@@ -2,48 +2,95 @@ import requests
 import random
 import time
 import subprocess
+import threading
+
+running = True
+start = False
 
 # --------------------------------------------------------------------------------------------------------------------------------------------- #
 
+print("Welcome to PyLove! To get started Connect your Phone and Computer to the same Wifi.")
+print("Then go into your Lovense Remote App and go to Settings>Game Mode. Connect your toy(s), then you will find the needed info there.")
+print()
+
 # Lovense Info Grabbing
 
-domain = input("Input your Phone's Local API:")
-httpPort = input("")
+while True:
+    print()
+    print("Input your Phone's Local API shown in the Lovense App: ")
+    domain = input(">>> ")
 
-api_url = f"http://{domain}:{httpPort}/command"
-print(api_url)
+    print("Input the Http Port shown on the Application.")
+    httpPort = input(">>> ")
+
+    api_url = f"http://{domain}:{httpPort}/command"
+    print(api_url)
+
+    if (domain != "" and httpPort != ""):
+        print(f"IP: {domain}\nHttp: {httpPort}")
+        confirm = input("^ ^ ^ Does this look Correct? Y/N >>> ")
+
+        if(confirm.capitalize() == "Y"):
+            break
+    else:
+        print("Invalid Input, values cannot be Empty.")
 
 
 def randomSurpriseHandler():
+    global running
+    global start
+
     print("Input how many seconds will occur between")
     intervalSec = int(input())
     print(f"You Have selected {intervalSec} seconds.")
 
-    while True:
+    print("\n \n ")
+    input("Press Enter to Start with these Settings:")
+
+    start = True
+
+    while running:
+        if not running:
+            break
+
         print("Looped")
 
-        randRoulette = random.randint(1,16)
+        randRoulette = random.randint(1,10)
         print(f"Roulette Roll: {randRoulette}")
 
-        if randRoulette == 4:
-            random_number = random.randint(1, 5)
+        if randRoulette == 5:
+            # Randomizes Activation Strength
+            random_number = random.randint(1, 10)
             print("Lvl1: Strength of Activation:" + str(random_number))
-        elif randRoulette == 8:
-            random_number = random.randint(5, 10)
+
+            # Randomizes Activation Duration
+            rand_time = random.randint(2, 8)
+            print(f"Duration: {rand_time} seconds")
+
+            RussianRoulette(random_number, rand_time)
+        elif randRoulette == 10:
+            # Randomizes Activation Strength
+            random_number = random.randint(10, 20)
             print("Lvl2: Strength of Activation:" + str(random_number))
-        elif randRoulette == 12:
-            random_number = random.randint(10, 15)
-            print("Lvl3: Strength of Activation:" + str(random_number))
-        elif randRoulette == 16:
-            random_number = random.randint(15, 20)
-            print("Lvl4: Strength of Activation:" + str(random_number))
+
+            # Randomizes Activation Duration
+            rand_time = random.randint(2, 8)
+            print(f"Duration: {rand_time} seconds")
+
+            RussianRoulette(random_number, rand_time)
         else:
             print('You got Lucky this time.')
+        print()
+
+        if not running:
+            break
 
         time.sleep(intervalSec)
 
+    print("Randomization cycle stopped.")
 
-def RussianRoulette(Strength: float, timesec: int):
+
+def RussianRoulette(Strength: int, timesec: int):
     toyCommand = {
         "command": "Pattern",
         "rule": "V:1;F:v,r,p,f,s,t;S:200#",
@@ -57,4 +104,13 @@ def RussianRoulette(Strength: float, timesec: int):
 
     response.status_code
 
-randomSurpriseHandler()
+surprise_thread = threading.Thread(target=randomSurpriseHandler)
+surprise_thread.start()
+
+while not start:
+    time.sleep(1)
+
+input("Press Enter to stop the randomization cycle.")
+print("Might take a moment to Restart.")
+running = False
+surprise_thread.join()
