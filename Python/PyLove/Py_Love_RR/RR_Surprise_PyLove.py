b7b545f3bb232efb73fd3f0a259224cd093609f4
UnLuckiestClovr <urluckyclovr@gmail.com>
2023-10-18 23:46:40 -0600

    Fixed Surprise Game, Bug involving Stopping Found

--- Python/PyLove/Py_Love_RR/RR_Surprise_PyLove.py
+++ Python/PyLove/Py_Love_RR/RR_Surprise_PyLove.py
@@ -1,11 +1,21 @@
 import requests
 import random
 import time
-import subprocess
 import threading
+import msvcrt
 
 running = True
 start = False
+api_url = ""
+
+def is_key_pressed():
+    # Check if a key has been pressed
+    return msvcrt.kbhit()
+
+def get_key():
+    # Get the key that was pressed
+    key = msvcrt.getch()
+    return key
 
 # --------------------------------------------------------------------------------------------------------------------------------------------- #
 
@@ -15,40 +25,55 @@ print()
 
 # Lovense Info Grabbing
 
-while True:
-    print()
-    print("Input your Phone's Local API shown in the Lovense App: ")
-    domain = input(">>> ")
+def pyLove_Surprise():
+    global running
+    global start
 
-    print("\nInput the Http Port shown on the Application.")
-    httpPort = input(">>> ")
-
-    api_url = f"http://{domain}:{httpPort}/command"
-    print(api_url)
-
-    if (domain != "" and httpPort != ""):
-        print(f"\n\nIP: {domain}\nHttp: {httpPort}")
-        confirm = input("\n\n^ ^ ^ Does this look Correct? Y/N >>> ")
-
-        if(confirm.capitalize() == "Y"):
-            break
-    else:
-        print("Invalid Input, values cannot be Empty.")
-
-def randomSurpriseHandler():
-        global running
-        global start
-
-        print("Input how many seconds will occur between")
+    while True:
+        print("\n\nInput how many seconds will occur between")
         intervalSec = int(input())
         print(f"You Have selected {intervalSec} seconds.")
 
-        print("\n \n ")
-        input("Press Enter to Start with these Settings:")
+        if isinstance(intervalSec, int):
+            break
+        else:
+            print("Invalid Input")
 
-        start = True
+    while True:
+        if not start:
+            print("Press Enter to start the randomization cycle.")
+            input()
+            start = True
 
+        surprise_thread = threading.Thread(target=surp_GameHandler(intervalSec))
+        surprise_thread.start()
+
+        print("Press Esc to stop the randomization cycle.")
         while running:
+            if is_key_pressed():
+                key = get_key()
+                if key == b'\x1b':  # Check for Esc key
+                    running = False
+            time.sleep(0.1)  
+
+        print("Might take a moment to Restart.")
+        surprise_thread.join()
+
+        print("Do you wish to Restart Surprise Round [y/n]?")
+        restart = input(">>> ").upper()
+
+        if restart=="N":
+            print("\n\n\n\n\n\n\n\n\n")
+            break
+        else:
+            print("Restarting app. . .")
+            running = True
+            start = False
+
+def surp_GameHandler(intervalSec : int):
+    global running
+
+    while running:
             if not running:
                 break
 
@@ -86,9 +111,11 @@ def randomSurpriseHandler():
 
             time.sleep(intervalSec)
 
-        print("Randomization cycle stopped.")
+    print("Randomization cycle stopped.")
+
 
 def Surprise_Random(Strength: int, timesec: int):
+    global api_url
     toyCommand = {
         "command": "Pattern",
         "rule": "V:1;F:v,r,p,f,s,t;S:200#",
@@ -103,26 +130,41 @@ def Surprise_Random(Strength: int, timesec: int):
     response.status_code
 
 
-# Handle Game Functions Below # 
+def main():
+    global api_url
 
-while True:
-    surprise_thread = threading.Thread(target=randomSurpriseHandler())
-    surprise_thread.start()
+    while True:
+        print()
+        print("Input your Phone's Local API shown in the Lovense App: ")
+        domain = input(">>> ")
 
-    while not start:
-        time.sleep(1)
+        print("\nInput the Http Port shown on the Application.")
+        httpPort = input(">>> ")
 
-    input("Press Enter to stop the randomization cycle.")
-    print("Might take a moment to Restart.")
-    running = False
-    surprise_thread.join()
+        api_url = f"http://{domain}:{httpPort}/command"
+        print(api_url)
 
-    print("Do you wish to Restart[R] or End[E or Quit] the Application? Type the respective [<Letter/Quote>]")
-    restart = input(">>> ")
-    restart = restart.capitalize()
-    if(restart == "E" or restart == "QUIT"):
-        print("\n\n\n\n\n\n\n\n\n")
-        break
-    else:
-        print("restarting app. . .")
-        running = True
+        if (domain != "" and httpPort != ""):
+            print(f"\n\nIP: {domain}\nHttp: {httpPort}")
+            confirm = input("\n\n^ ^ ^ Does this look Correct? Y/N >>> ")
+
+            if(confirm.capitalize() == "Y"):
+                break
+        else:
+            print("Invalid Input, values cannot be Empty.")
+
+    notClosed = True
+    while notClosed:
+        
+        print("Which Game would you like to Play?\n 1 : Surprise! \nQUIT : Close PyLove")
+        gameOption = input(">>> ")
+        gameOption = gameOption
+
+        match gameOption:
+            case "1":
+                pyLove_Surprise()
+            case "QUIT":
+                break
+
+
+main()
